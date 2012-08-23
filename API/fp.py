ca1cd20db97384cfcb941113fae5810741fa8919
Ben Good <bgood@clipinteractive.com>
2012-08-23 12:08:13 -0600

    Commentted debug message that could throw exceptions.

--- API/fp.py
+++ API/fp.py
@@ -206,7 +206,7 @@ def best_match_for_query(code_string, elbow=10, local=False):
             continue
         actual_scores[track_id] = actual_matches(code_string, track_code, elbow = elbow)
     
-    logger.debug("Actual score for %s is %d (code_len %d), original was %d" % (r["track_id"], actual_scores[r["track_id"]], code_len, top_match_score))
+    #logger.debug("Actual score for %s is %d (code_len %d), original was %d" % (r["track_id"], actual_scores[r["track_id"]], code_len, top_match_score))
     # Sort the actual scores
     sorted_actual_scores = sorted(actual_scores.iteritems(), key=lambda (k,v): (v,k), reverse=True)
     
